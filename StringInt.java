e8ad49773367d6a6a90baab36a9f270c56f39d72
tim-mei <tmei30@stuy.edu>
2021-12-21 18:23:05 -0500

    What I did in class; could not push because of remote verification or something

---
+++ StringInt.java
@@ -0,0 +1,17 @@
+
+public class StringInt{
+
+  /*public static int stringToInt(String s){
+        //return the value of Integer.parseInt(s) but do it yourself!
+        //Do not use any built in parse methods.
+      } */
+
+  public static int valueOfDigit(char c){
+    int value = c;
+    return c - 48;
+  }
+
+  public static void main(String[] args) {
+    System.out.println('4');
+  }
+}
