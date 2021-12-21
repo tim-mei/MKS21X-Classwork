468938ed858f396ef144bf612557d5dba6639739
tim-mei <tmei30@stuy.edu>
2021-12-21 18:34:33 -0500

    Works on negatives and tested 0

--- StringInt.java
+++ StringInt.java
@@ -6,8 +6,15 @@ public class StringInt{
         //Do not use any built in parse methods.
     int value = 0;
     for(int k=0; k<s.length(); k++){
+      //look for the minus sign, then multiply the next value by -1
       char in = s.charAt(k);
-      value+=valueOfDigit(in);
+      if(in == '-'){
+        value+=valueOfDigit(s.charAt(k+1))*-1;
+        k+=1;
+      }
+      else{
+        value+=valueOfDigit(in);
+      }
     }
     return value;
   }
@@ -20,5 +27,8 @@ public class StringInt{
   public static void main(String[] args) {
     System.out.println('4');
     System.out.println("Should return 17: " + stringToInt("041705"));
+    System.out.println("Should return -8: " + stringToInt("-8"));
+    System.out.println("Should return -5: " + stringToInt("0-41-705"));
+    System.out.println("Should return 0: " + stringToInt("0"));
   }
 }
