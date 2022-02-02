e39712e0a096413a93d776104379e14da2f5f8c1
tim-mei <tmei30@stuy.edu>
2022-02-02 17:59:33 -0500

    prints aaa, but skips to abc and only prints out 2 three letter words

--- Makewords.java
+++ Makewords.java
@@ -1,14 +1,18 @@
-public class MakeWords{
+public class Makewords{
 
   public static void main(String[] args) {
     makeWords(3,"");
-    for(char c = 'a'; c<='z'; c++){
+    for(char c = 'a'; c<='c'; c++){
       System.out.print(c);
     }
     System.out.println();
   }
 
   public static void makeWords(int size, String ans){
-    if(ans.length() == size && )
+    if(size == 0){
+      System.out.println(ans);
+    }else{
+      makeWords(size - 1, ans + 'a');
+    }
   }
 }
