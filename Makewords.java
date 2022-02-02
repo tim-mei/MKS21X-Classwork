d53320c57f5ba2666e8e65a121e3ead3f9d9d5a0
tim-mei <tmei30@stuy.edu>
2022-02-02 17:47:32 -0500

    Couldn't push in class, git related problems

---
+++ Makewords.java
@@ -0,0 +1,14 @@
+public class MakeWords{
+
+  public static void main(String[] args) {
+    makeWords(3,"");
+    for(char c = 'a'; c<='z'; c++){
+      System.out.print(c);
+    }
+    System.out.println();
+  }
+
+  public static void makeWords(int size, String ans){
+    if(ans.length() == size && )
+  }
+}
