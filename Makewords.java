31cf8349d34eba3554ec5f9b5599f0d2e45847e8
tim-mei <tmei30@stuy.edu>
2022-02-02 18:42:38 -0500

    portion given in class goes inside makewords; the looping appears correct?

--- Makewords.java
+++ Makewords.java
@@ -1,18 +1,16 @@
 public class Makewords{
 
   public static void main(String[] args) {
-    makeWords(3,"");
-    for(char c = 'a'; c<='c'; c++){
-      System.out.print(c);
-    }
-    System.out.println();
+    makeWords(4,"");
   }
 
   public static void makeWords(int size, String ans){
     if(size == 0){
       System.out.println(ans);
     }else{
-      makeWords(size - 1, ans + 'a');
+      for(char c = 'a'; c<='z'; c++){
+        makeWords(size - 1, ans + c);
+      }
     }
   }
 }
